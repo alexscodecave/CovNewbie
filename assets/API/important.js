590e0e3e28154cdf930241508978ee2785fb783e
Bids <alexbiddle@Alexs-MacBook-Air.local>
2019-06-19 21:48:42 +0100

    changed git ignore

--- assets/API/important.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    secret:'AIzaSyAr1j3xWW2kSbtek-8cI9XR3q2AIDjNxQ0', //google api key
-    ticketMasterConsumer:'0GxqB8yabGwRCGn74LEzfjwZpNGg5CqQ',
-    ticketMasterSecret:'3kVii575vTNlXaEf'
-}
